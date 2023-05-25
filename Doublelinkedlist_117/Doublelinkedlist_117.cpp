da179e7d88545ba8517b906d5fabef0c725eb819
nauvalachid <nauvalachid@gmail.com>
2023-05-25 15:59:12 +0700

    langkah 6

--- Doublelinkedlist_117/Doublelinkedlist_117.cpp
+++ Doublelinkedlist_117/Doublelinkedlist_117.cpp
@@ -70,3 +70,5 @@ void Doublelinkedlist::addNode() {
 
 	newNode->next = current->next; //step 4
 	newNode->prev = current; //step 5
+	if (current->next != NULL)
+		current->next->prev = newNode; //step 6
