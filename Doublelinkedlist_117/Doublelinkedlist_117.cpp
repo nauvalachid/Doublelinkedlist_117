ef101026a8c4b95810e2c6c0c679dda90d207fc2
nauvalachid <nauvalachid@gmail.com>
2023-05-25 15:41:06 +0700

    langkah 1

--- Doublelinkedlist_117/Doublelinkedlist_117.cpp
+++ Doublelinkedlist_117/Doublelinkedlist_117.cpp
@@ -28,3 +28,12 @@ public:
 Doublelinkedlist::Doublelinkedlist() {
 	START = NULL;
 }
+
+void Doublelinkedlist::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
