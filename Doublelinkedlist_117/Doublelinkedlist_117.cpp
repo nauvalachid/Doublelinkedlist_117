31769881ad58a140e8337f5008a936bdbb3cd15f
nauvalachid <nauvalachid@gmail.com>
2023-05-25 16:10:26 +0700

    langkah 1.e

--- Doublelinkedlist_117/Doublelinkedlist_117.cpp
+++ Doublelinkedlist_117/Doublelinkedlist_117.cpp
@@ -80,3 +80,7 @@ bool Doublelinkedlist::search(int rollNo, Node** previous, Node** current) {
 	*current = START; //step 1.b
 	while (*current != NULL && rollNo != (*current)->noMhs) { //step 1.c
 		*previous = *current; // step 1.d
+		*current = (*current)->next; //step 1.e
+	}
+	return (*current != NULL);
+}
