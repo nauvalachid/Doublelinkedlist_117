bea0f6d9425ee644d77a080977ceea4112efae13
nauvalachid <nauvalachid@gmail.com>
2023-05-25 19:53:53 +0700

    melakukan tindakan tertentu pada setiap elemen.

--- Doublelinkedlist_117/Doublelinkedlist_117.cpp
+++ Doublelinkedlist_117/Doublelinkedlist_117.cpp
@@ -104,3 +104,16 @@ bool Doublelinkedlist::deleteNode(int rollNo) {
 bool Doublelinkedlist::listEmpty() {
 	return (START == NULL);
 }
+
+void Doublelinkedlist::traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in ascending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
