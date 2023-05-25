668962813c18de693f1853bdab479d192e0903e0
nauvalachid <nauvalachid@gmail.com>
2023-05-25 15:41:48 +0700

    langkah 2

--- Doublelinkedlist_117/Doublelinkedlist_117.cpp
+++ Doublelinkedlist_117/Doublelinkedlist_117.cpp
@@ -37,3 +37,5 @@ void Doublelinkedlist::addNode() {
 	cout << "\nEnter the name of the student: ";
 	cin >> nm;
 	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; //step 2
+	newNode->name = nm; //step 2
